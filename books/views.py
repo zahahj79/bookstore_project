78f69fff591162f6477983b1a7db9e163c085e8d
zahra_hj <zahra.hajizadeh79ce@gmail.com>
2024-02-16 14:30:29 +0330

    add some template filter

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 from .models import Book
 
@@ -25,3 +26,9 @@ class BookUpdateView(generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'description']
     template_name = 'books/book_update.html'
+
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
